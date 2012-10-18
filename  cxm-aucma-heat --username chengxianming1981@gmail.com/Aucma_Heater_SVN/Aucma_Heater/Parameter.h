2700d8e6af18c7afda83265409e2fc2ce75fae1c
chengxianming1981 <chengxianming1981@8d4988cf-a08e-6cf0-31a8-6f5a1cebb841>
2012-10-18 05:32:02 +0000

    采用OnPaint消息响应中DC绘图的方式基本完成界面显示所需功能（需要将温度和时间的显示图标替换掉）。
    git-svn-id: https://cxm-aucma-heat.googlecode.com/svn/trunk@8 8d4988cf-a08e-6cf0-31a8-6f5a1cebb841

---
+++  cxm-aucma-heat --username chengxianming1981@gmail.com/Aucma_Heater_SVN/Aucma_Heater/Parameter.h
@@ -0,0 +1,28 @@
+#ifndef _Parameter_h
+#define _Parameter_h
+
+/** 定义对话框背景颜色*/
+#define DialogBkColor		(RGB(255, 255, 255))
+/** 默认文字颜色*/
+#define DefaultTextColor	(RGB(132, 128, 144))
+/** 选中文字颜色*/
+#define SelectTextColor		(RGB(255, 0, 0))
+/** 监控温度状态定时器序号*/
+#define ShowTempStateTimerEvent	1
+/** 监控温度状态定时间隔*/
+#define ShowTempStateTimeSet	1000
+/** 监控系统时间定时器序号*/
+#define ShowTimeTimerEvent		2
+/** 监控系统时间定时间隔*/
+#define ShowTimeTimeSet			10000
+/** 连续增加和减少定时器序号*/
+#define ContinuousOptTimerEvent		3
+/** 连续增加和减少定时间隔*/
+#define ContinuousOptTimeSet		100
+/** 点击增加和减少达到时间次数，用于判定连续操作*/
+#define ContinuousOptTimes			10
+// 速热引擎状态枚举
+enum {NormalHeat, FastHeat, WinterHeat};
+// 温度显示状态
+enum {NoTempShow, InsulationState, HeatTempState};
+#endif
